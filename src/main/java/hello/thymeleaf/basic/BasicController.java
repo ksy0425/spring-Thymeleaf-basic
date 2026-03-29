9f50bb62ee1de8e85da8efdf9bf6ff66fda92827
ksy0425 <180037120+isumrgb@users.noreply.github.com>
2026-03-29 14:44:03 +0900

    속성 값 설정(#2)

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -94,6 +94,11 @@ public class BasicController {
         return "basic/operation";
     }
 
+    @GetMapping("/attribute")
+    public String attribute() {
+        return "basic/attribute";
+    }
+
     @Data
     public static class User {
         private String username;
